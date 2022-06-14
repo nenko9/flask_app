29e8c56e50211045c4d65e182e6592f8d5791034
renko <96355820+nenko9@users.noreply.github.com>
2022-06-14 17:39:13 +0300

    add index page

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+# from crypt import methods
+from flask import Flask, render_template, request
+# export FLASK_DEBUG=1
+
 
 app = Flask(__name__)
+# app.run(debug=True)
+@app.route("/", methods=["GET", "POST"])
 
-@app.route("/")
-def hello_world():
-        return "<p>Hello, World!</p>"
+        
+def IndexPage():
+        # def Searcher():
+        #         return 'find'
+
+        if request.method=='POST':
+                # vv = request.form['search']
+                search_query = request.form['search_query']
+                # print('Post was reciev', vv)
+                # return render_template('index.html')
+        else:
+                return render_template('index.html')
+
+        return render_template('index.html')
+
+if __name__ =='__main__':
+        app.run()
